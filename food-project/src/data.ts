719ae382365e295384bdebcf68b20261078fd523
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-03-31 23:13:54 +0500

    food interface created and pics added to assets

---
+++ food-project/src/data.ts
@@ -0,0 +1,116 @@
+import { Food } from "./app/shared/models/Foods";
+
+export const sample_foods: Food[] = [
+  {
+    id: '1',
+    name: 'Chicken Soupt',
+    price: 9,
+    cookTime: '40-50',
+    favorite: false,
+    origins: ['india', 'asia'],
+    stars: 3.0,
+    imageUrl: 'assets/food-5.jpg',
+    tags: ['SlowFood', 'Soup']
+  },
+  {
+    id: '2',
+    name: 'Melon',
+    price: 9,
+    cookTime: '40-50',
+    favorite: false,
+    origins: ['india', 'asia'],
+    stars: 3.0,
+    imageUrl: 'assets/food-5.jpg',
+    tags: ['SlowFood', 'Soup']
+  },
+  {
+    id: '3',
+    name: 'Lavash',
+    price: 4,
+    cookTime: '40-50',
+    favorite: false,
+    origins: ['georgia', 'caucasus'],
+    stars: 3.0,
+    imageUrl: 'assets/food-5.jpg',
+    tags: ['FastFood', 'Lavash']
+  },
+  {
+    id: '4',
+    name: 'Rolton',
+    price: 9,
+    cookTime: '10-20',
+    favorite: false,
+    origins: ['russia', 'europe'],
+    stars: 3.0,
+    imageUrl: 'assets/food-5.jpg',
+    tags: ['FastFood', 'Rolton']
+  },
+  {
+    id: '5',
+    name: 'Cesar',
+    price: 13,
+    cookTime: '40-50',
+    favorite: false,
+    origins: ['italy', 'europe'],
+    stars: 3.0,
+    imageUrl: 'assets/food-5.jpg',
+    tags: ['Salad', 'Cesar']
+  },
+  {
+    id: '6',
+    name: 'Olivie',
+    price: 12,
+    cookTime: '40-50',
+    favorite: false,
+    origins: ['italy', 'europe'],
+    stars: 3.0,
+    imageUrl: 'assets/food-5.jpg',
+    tags: ['Salat', 'Olivie']
+  },
+  {
+    id: '7',
+    name: 'Hamburger',
+    price: 9,
+    cookTime: '40-50',
+    favorite: false,
+    origins: ['usa', 'north america'],
+    stars: 3.0,
+    imageUrl: 'assets/food-5.jpg',
+    tags: ['FastFood', 'Hamburger']
+  },
+  {
+    id: '8',
+    name: 'Ratatul',
+    price: 50,
+    cookTime: '40-50',
+    favorite: false,
+    origins: ['france', 'europe'],
+    stars: 3.0,
+    imageUrl: 'assets/food-5.jpg',
+    tags: ['SlowFood', 'Ratatul']
+  },
+  {
+    id: '9',
+    name: 'Spaggeti',
+    price: 20,
+    cookTime: '40-50',
+    favorite: false,
+    origins: ['italy', 'europe'],
+    stars: 3.0,
+    imageUrl: 'assets/food-5.jpg',
+    tags: ['SlowFood', 'Spaggeti']
+  },
+  {
+    id: '10',
+    name: 'Palow',
+    price: 20,
+    cookTime: '40-50',
+    favorite: false,
+    origins: ['uzbekistan', 'asia'],
+    stars: 3.0,
+    imageUrl: 'assets/food-5.jpg',
+    tags: ['SlowFood', 'Palow']
+  }
+
+
+]
