a65c00af444a204b92fe286f1a9abf11424fa80a
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-04-17 22:07:32 +0500

    Added login page component

--- food-project/src/app/app.module.ts
+++ food-project/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { FoodPageComponent } from './components/pages/food-page/food-page.compon
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
 import { TitleComponent } from './components/partials/title/title.component';
 import { NotFoundComponent } from './components/partials/not-found/not-found.component';
+import { LoginComponent } from './components/pages/login/login.component';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { NotFoundComponent } from './components/partials/not-found/not-found.com
     FoodPageComponent,
     CartPageComponent,
     TitleComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
