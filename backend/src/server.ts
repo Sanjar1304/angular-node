6c448ee0e2c261f1d3797a96438b269a841d1f59
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-04-17 21:16:19 +0500

    First backend connection is finished

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -23,8 +23,21 @@ app.get('/api/foods/tags', (req, res) => {
     res.send(sample_tags)
 })
 
+
+app.get('/api/foods/tag/:tagName', (req, res) => {
+    const tagName = req.params.tagName;
+    const foods = sample_foods.filter(food => food.tags?.includes(tagName))
+    res.send(foods);
+})
+
+
+app.get('/api/foods/:foodId', (req, res) => {
+    const foodId = req.params.foodId
+    const food = sample_foods.find(food => food.id == foodId)
+    res.send(food);
+})
+
 const port = 5000;
 app.listen(port, () => {
     console.log('website served on http://localhost:' + port);
-    
 })
