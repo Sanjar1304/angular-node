db242271a3759215a61e98e8e7c03c40b7aaa869
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-05-02 23:24:09 +0500

    Added input validation not full

--- food-project/src/app/components/partials/input-validation/input-validation.component.ts
+++ food-project/src/app/components/partials/input-validation/input-validation.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { AbstractControl } from '@angular/forms';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AbstractControl, Validators } from '@angular/forms';
 
 const VALIDATORS_MESSAGE: any = {
   required: 'Should not be empty',
@@ -7,11 +7,11 @@ const VALIDATORS_MESSAGE: any = {
 }
 
 @Component({
-  selector: 'app-input-validation',
+  selector: 'input-validation',
   templateUrl: './input-validation.component.html',
   styleUrls: ['./input-validation.component.css']
 })
-export class InputValidationComponent implements OnInit {
+export class InputValidationComponent implements OnInit, OnChanges {
 
   @Input() control!: AbstractControl
   @Input() showErrorsWhen: boolean = true
@@ -19,11 +19,28 @@ export class InputValidationComponent implements OnInit {
 
   constructor() { }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    this.checkValidation()
+  }
+
   ngOnInit(): void {
+    this.control.statusChanges.subscribe(() => {
+      this.checkValidation()
+    })
+    this.control.valueChanges.subscribe(() => {
+      this.checkValidation();
+    })
   }
 
   checkValidation():void{
+    const errors = this.control.errors;
+    if(!errors){
+      this.errorMessages = [];
+      return;
+    }
 
+    const errorKeys = Object.keys(errors);
+    this.errorMessages = errorKeys.map(key => VALIDATORS_MESSAGE[key])
   }
 
 }
